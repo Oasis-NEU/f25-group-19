de42abc33677f68f1088d7729c8c3e7539021bc2
abhijosyu <abhijosyu@gmail.com>
2025-10-05 13:13:27 -0400

    removed src files

--- tracker/src/App.tsx
+++ tracker/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,11 +6,6 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
