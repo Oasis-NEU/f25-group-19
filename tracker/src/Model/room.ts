fceb4338168e5e4eb6b15571827510a8b3fa8bee
Luis Li <luisthecool8@gmail.com>
2025-10-26 12:52:09 -0400

    fixed formatting issue

--- tracker/src/Model/room.ts
+++ tracker/src/Model/room.ts
@@ -6,18 +6,18 @@ class room {
     room_code: number;
     chore_list: Map<string, Chores>; // this means chore title has to be unique since string will be chore.task_title
 
-    
-    
+
+
     constructor(users: User[], room_code: number) {
         this.users = users;
         this.room_code = room_code;
         this.chore_list = new Map<string, Chores>(); // change this once impelmentation is done 
     }
 
-       // checks if the given chore title is in the room
-       chore_exists(chore: string): boolean {
+    // checks if the given chore title is in the room
+    chore_exists(chore: string): boolean {
         return this.chore_list.has(chore)
-     }
+    }
 
 
     /**
@@ -27,27 +27,27 @@ class room {
      * @param change the actual value to change the element to
      */
     edit_specific_chore(chore: string, element: string, change: any): void {
-        if(this.chore_exists(chore) == true) {
-            switch(element) {
+        if (this.chore_exists(chore) == true) {
+            switch (element) {
                 case 'TITLE': // make sure controller has this exact string for element and for the rest
-                    if(this.chore_exists(change) == false) {
+                    if (this.chore_exists(change) == false) {
                         this.chore_list.get(chore)?.edit_task(change)
                     } else {
                         throw new Error('chore title already exists')
                     }
                     break;
-                case 'DESCRIPTION': 
+                case 'DESCRIPTION':
                     this.chore_list.get(chore)?.edit_description(change)
                     break;
-                case 'FREQUENCY': 
-                    if(change instanceof Date) {
+                case 'FREQUENCY':
+                    if (change instanceof Date) {
                         this.chore_list.get(chore)?.edit_frequency(change)
                     } else {
                         throw new Error('not a valid Date')
                     }
                     break;
                 case 'COMPLETION':
-                    if(change == true || change == false) {
+                    if (change == true || change == false) {
                         this.chore_list.get(chore)?.edit_completion(change)
                     } else {
                         throw new Error('not a valid boolean')
@@ -58,14 +58,13 @@ class room {
             throw new Error('chore does not exist')
         }
 
-        }
-
     }
 
- 
-
-
-    // default chore_list needs to be implemented 
-
-
 }
+
+
+
+
+// default chore_list needs to be implemented 
+
+
